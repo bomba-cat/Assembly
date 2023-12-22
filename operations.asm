1f91d36c6e0d6959ab34ec9173b131e39e30c1c0
xk_rl <85764488+xk-rl@users.noreply.github.com>
2023-12-22 22:34:10 +0100

    calls and mathematical operations

---
+++ operations.asm
@@ -0,0 +1,40 @@
+section .data
+	digit db 0,10
+
+section .text
+	global _start
+
+_start:
+	mov rax, 6
+	mov rbx, 2
+	div rbx
+	call _printRAXDigit
+
+	mov rax, 1
+	add rax, 8
+	call _printRAXDigit
+	
+	push 4
+	push 8
+	push 3
+
+	pop rax
+	call _printRAXDigit
+	pop rax
+	call _printRAXDigit
+	pop rax
+	call _printRAXDigit
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_printRAXDigit:
+	add rax, 48
+	mov [digit], al
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, digit
+	mov rdx, 2
+	syscall
+	ret
